fa7205bacc67f714f570b02a497d32993ef92380
notsight <omtripathi177@gmail.com>
2025-04-19 13:25:51 +0530

    frontend changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import styled, { ThemeProvider } from "styled-components";
+import { darkTheme } from "./utils/Theme";
+// import { Container } from "@mui/material";
+import Home from "./pages/home";
+import CreatePost from "./pages/CreatePost";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  background-color: ${({ theme }) => theme.bg};
+  color: ${({ theme }) => theme.text_primary};
+  overflow-x: hidden;
+  overflow-y: hidden;
+  transition: all 0.2s ease;
+`
+
+const Wrapper = styled.div`
+  height: 100%;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  flex: 3;
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <Container>
+        <Wrapper>
+          <BrowserRouter>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/post" element={<CreatePost />} />
+            </Routes>
+          </BrowserRouter>
+          <Home />
+          <CreatePost />
+        </Wrapper>
+      </Container>
+    </ThemeProvider>
   );
 }
 
